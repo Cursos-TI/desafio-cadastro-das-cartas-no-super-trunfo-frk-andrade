4a1e6dbb76b0ce90d2d385c8f9bd86660da90c8a
Franklin Andrade <100174287+frk-andrade@users.noreply.github.com>
2025-06-11 13:50:59 +0000

    Exibição dos dados armazenados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -43,6 +43,15 @@ int main() {
     scanf("%f", &pib1);
     printf("Informe a quantidade de pontos turísticos: \n");
     scanf("%d", &p_turistico1);
-    
+    //Exibição dos dados armazenados
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", letra_estado1);
+    printf("Código: %s\n", cod_carta1);
+    printf("Nome da Cidade: %s\n", nome_cidade1);
+    printf("População: %d\n", habitantes1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d \n", p_turistico1);
+
     return 0;
 }
